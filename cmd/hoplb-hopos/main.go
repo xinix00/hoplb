ea56857d9d9ea527aab71b6301206f7c23009650
Derek den Haas <d.haas@directcode.com>
2026-09-03 21:41:30 +0200

    metal wordt module /v2
    Go eist vanaf major 2 dat het modulepad de major draagt, dus HopOS' metal heet
    nu github.com/xinix00/HopOS/metal/v2. Alleen het adres verandert: zelfde code,
    zelfde gedrag, en de pin gaat mee naar v2.0.0.

--- cmd/hoplb-hopos/main.go
+++ cmd/hoplb-hopos/main.go
@@ -28,8 +28,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/xinix00/HopOS/metal/app/applib"
-	"github.com/xinix00/HopOS/metal/app/applib/appnet"
+	"github.com/xinix00/HopOS/metal/v2/app/applib"
+	"github.com/xinix00/HopOS/metal/v2/app/applib/appnet"
 
 	"github.com/xinix00/hoplb/internal/lb"
 	"github.com/xinix00/hoplb/internal/metrics"
